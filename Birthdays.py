34dfcdd7c9d8a791d5ab327e509c5d93faff63d1
varman897 <varmansivarooban2015@gmail.com>
2026-01-12 20:58:51 -0800

    Birthdays

---
+++ Birthdays.py
@@ -0,0 +1,11 @@
+varman_birthday = "May 1, 2015"
+mervan_birthday = "February 12, 2013"
+mom_birthday = "December 28, 1977"
+dad_birthday = "October 21, 1969"
+messi_birthday = "June 24, 1987"
+
+print("Varman's Birthday:", varman_birthday)
+print("Mervan's Birthday:", mervan_birthday)
+print("Mom's Birthday:", mom_birthday)
+print("Dad's Birthday:", dad_birthday)
+print("Lionel Messi's Birthday:", messi_birthday)
